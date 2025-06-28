198376bdf41ee939206500475b17fdd1cb3da889
Abbas Aga <garderappstore@gmail.com>
2025-06-28 19:20:26 +0300

    fix hydration

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useEffect, useState } from 'react';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/app-sidebar';
 import { useUser } from '@/components/user-provider';
@@ -11,10 +12,18 @@ export default function DashboardLayout({
 }) {
   const { user, signOut } = useUser();
   const router = useRouter();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const handleSignOut = () => {
     router.push('/');
     signOut();
   };
+
+  if (!mounted) return null; // SSR ile uyuşmazlık olmasın diye
 
   if (!user) {
     return (
@@ -28,8 +37,8 @@ export default function DashboardLayout({
     <SidebarProvider>
       <div className="flex w-full min-h-screen">
         <AppSidebar onSignOut={handleSignOut} />
-        <main className="flex-1 p-2 flex justify-center items-start  overflow-auto">
-          <div className="w-full  border border-border shadow-sm dark:shadow:sm rounded-xl h-full px-4 py-4 bg-white dark:bg-[#0A0A0A]">
+        <main className="flex-1 p-2 flex justify-center items-start overflow-auto">
+          <div className="w-full border border-border shadow-sm dark:shadow:sm rounded-xl h-full px-4 py-4 bg-white dark:bg-[#0A0A0A]">
             <SidebarTrigger />
             {children}
           </div>
