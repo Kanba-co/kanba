d9313f41ef62846faa09d340301e875bd8fb49f4
Abbas Aga <garderappstore@gmail.com>
2025-06-28 14:49:36 +0300

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -338,7 +338,7 @@ export default function Home() {
                     rel="noopener noreferrer"
                   >
                     <GithubIcon className="h-5 w-5" />
-                    View on GitHuba
+                    View on GitHubas
                   </a>
                 </Button>
               </div>
